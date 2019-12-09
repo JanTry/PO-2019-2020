6b12400cd35391f3580203c09ab454590054b445
JanTry <44120531+WDIjantry@users.noreply.github.com>
2019-12-09 13:38:03 +0100

    working

--- Project/untitled/src/com/company/Animal.java
+++ Project/untitled/src/com/company/Animal.java
@@ -36,17 +36,37 @@ public class Animal {
     public int getY(){
         return this.position.getY();
     }
+    public Vector2d getPosition(){
+        return this.position;
+    }
     public void rotate(int number){
         for(int i=0;i<number;i++){
             this.direction=this.direction.next();
         }
     }
-    public Animal move(Vector2d move){
-        this.position=this.position.add(move);
+    public Animal moveForward() {
+        this.position = this.position.add(this.direction.toUnitVector());
 //        Here we shall move it on the map
-//        And probably a few cases will be here - covering beeing unable to move
+//        And probably a few cases will be here - covering being unable to move
 //        Also important to remember about removing animal on the opposite side of the map
 //        In case of moving out of boundaries
         return this;
+    }
+    public void eatGrass(){
+        this.energy+=10;
+    }
+    public boolean process(){
+//        It has to:
+//        Rotate
+//        Check if can move forward
+//        If yes, Move
+//        If there is grass - eat it
+//        If there is animal - have sex with it
+
+        this.rotate(this.genes.getRotation());
+
+
+        this.energy--;
+        return energy > 0;
     }
 }
--- Project/untitled/src/com/company/Main.java
+++ Project/untitled/src/com/company/Main.java
@@ -1,9 +1,9 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
 	// write your code here
+
         System.out.println("It does compile");
         System.out.println("At least for now");
     }
